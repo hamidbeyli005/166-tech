a6046ab66cadcd34683481026a922946064c41ed
Hamid Hamidbayli <hamidhamidbayli@Hamids-MacBook-Pro.local>
2024-02-17 10:29:22 +0400

    Added media queries for resposive pages

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var hamburgerMenu = document.querySelector('.hamburger-menu-icon');
-    var menu = document.querySelector('.hamburger-menu');
+    let hamburgerMenu = document.querySelector('.hamburger-menu-icon');
+    let menu = document.querySelector('.hamburger-menu');
+    let items = document.querySelectorAll('.hamburger-menu ul li.menu-item')
 
-    hamburgerMenu.addEventListener('click', function () {
+    hamburgerMenu.addEventListener('click', () => {
         menu.classList.toggle('open');
     });
+
+    items.forEach(li => {
+        li.addEventListener('click', () => {
+            menu.classList.toggle('open');
+        })
+    })
+
 });
